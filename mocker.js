aab254cd998aae0a2b5febf755a3af9b7962e23f
Sahar Soffer <saharsoffer@Sahars-MacBook-Pro.local>
2021-10-14 06:47:50 +0300

    add delete and add files support

--- mocker.js
+++ mocker.js
@@ -1,9 +1,9 @@
-console.log('haudy');
 const fs = require('fs');
 
-// const makeMocksDir = require('./utils/makeDir').makeMocksDir
+const makeDir = require('./utils/makeDir').makeMocksDir
 //
-// makeMocksDir('./mocks');
+makeDir('./mocks');
+makeDir('./schemas');
 //
 //
 // let schema = {
@@ -17,9 +17,7 @@ const fs = require('fs');
 // let data = JSON.stringify(schema);
 // fs.writeFileSync('./mocks/student-2.json', data);
 
-const Joi = require('joi');
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv/config');
 const app = express();
 app.use(express.json());
@@ -27,6 +25,14 @@ app.use(express.json());
 // Routes
 // const postRoutes = require('./routes/posts');
 // app.use('/api/posts', postRoutes);
+
+
+function getFiles(path, callback){
+    fs.readdir(path, (err, files) => {
+        const parsedFiles = files.map((file, index) => ({id: index, name: file.split('.')[0]}))
+        callback(parsedFiles)
+    });
+}
 
 const courses = [
     {id: 1, name: 'course 1'},
@@ -41,10 +47,31 @@ app.get('/', (req, res) => {
 
 });
 
-app.post('/api', (req, res) => {
+app.post('/api/addSchema', (req, res) => {
     console.log(req.body);
+
+    const raw = JSON.stringify(req.body);
+    const data = JSON.parse(raw);
+
+    const fileName = data.fileName;
+    const fileContent = JSON.parse(data.value)
+
+    // console.log(data);
+
+    fs.writeFileSync(`./schemas/${fileName}.json`, JSON.stringify(fileContent));
     res.send(JSON.stringify(req.body));
 });
+
+app.get('/api/getSchemaFiles', (req, res)=>{
+    getFiles('./schemas', (files)=>{
+        res.send(JSON.stringify(files));
+    })
+});
+app.get('/api/getSchemaFile/:id',(req,res)=>{
+    const buffer = fs.readFileSync(`./schemas/${req.params.id}.json`);
+    console.log(buffer.toString())
+    res.send(JSON.stringify(buffer.toString()));
+})
 
 const port = 1010;
 app.listen(port, ()=> console.log(`listening on port ${port}...`));
